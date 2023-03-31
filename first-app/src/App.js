48b8b536625612cc48b9deaea6ac8af52dfd1243
saranyaveeradurai <saranya@dcsipl.com>
2023-03-31 23:46:19 +0530

    react-boostrap added

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,55 +1,27 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import React from 'react';
-
-
-function Header() {
+import 'bootstrap/dist/css/bootstrap.css';
+import {Card}  from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import Img1 from './logo192.png'
+export default function App(props) {
   return (
-    <div> Header</div>
-  )
-}
-function Container() {
-  return (
-    <div> Container</div>
-  )
-}
-function Footer() {
-  return (
-    <div> Footer</div>
-  )
-}
-export default function App() {
-  return (
-    <div>
-      <Header/>
-      <Container/>
-      <Footer/>
-    </div>
+    // <div>
+    //     <h1>PROPERTIES</h1>
+    //     <p>Hiiii {props.name}</p>
+        
+    // </div>
+    <>
+    <Card style={{ width: '18rem'}}>
+    <Card.Img variant="top" src={Img1} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </>
   )
 }
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -4,7 +4,7 @@ import App from './App'
 function Index() {
   return (
     <div>
-      <App/>
+<App /> 
     </div>
   )
 }
