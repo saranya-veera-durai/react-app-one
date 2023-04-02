e4f3b0b54645c0b500e8aed03cd8aa4f5bd67dd7
saranyaveeradurai <saranya@dcsipl.com>
2023-04-02 21:25:46 +0530

    arrow function

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import {Card}  from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import Img1 from './logo192.png'
-export default function App(props) {
-  return (
-    // <div>
-    //     <h1>PROPERTIES</h1>
-    //     <p>Hiiii {props.name}</p>
-        
-    // </div>
+
+const Header=()=>{
+  return(
     <>
-    <Card style={{ width: '18rem'}}>
-    <Card.Img variant="top" src={Img1} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-          {props.text}
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <h1>hiiii</h1>
     </>
   )
 }
+export default Header
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import Header from './App'
 function Index() {
   return (
     <div>
-<App title="titleone" text="Welcome all"/> 
-<App title="titletwo" text="Welcome "/> 
-<App title="titlethree" text="Welcome everyone"/> 
+<Header/>
     </div>
   )
 }
