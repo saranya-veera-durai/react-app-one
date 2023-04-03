9c693b7eda736be2edf8decf521d630e8394c9a0
saranyaveeradurai <saranya@dcsipl.com>
2023-04-03 21:46:29 +0530

    useCallback

--- first-app/src/components/Product.js
+++ first-app/src/components/Product.js
@@ -3,7 +3,7 @@ const Product=React.memo((props)=> {
     console.log(`${props.property} re-rendered`)
   return (
     <div>
-        <h1>{props.property}</h1>
+        <h1 onClick={props.cart}>{props.property}</h1>
     </div>
   )
 })
--- first-app/src/components/useMemo.js
+++ first-app/src/components/useMemo.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import Product from './Product'
 export default function UseMemo() {
-    const products =["product1","product2","product3"]
+    const products =["product1","product2","product3",]
     const [count,setCount]=useState(0)
+    const [cart,setCart]=useState(0)
+    const addToCart=useCallback(()=>{
+        setCart(cart+1)
+    },[cart])
   return (
     <div>
-        <h1>{count}</h1>
+        <h1>COUNT:{count}</h1>
         <button onClick={()=>{setCount(count+1)}}>increment</button>
 
+        <h1>CART:{cart}</h1>
         {products.map((product)=>{
             return(
-                <Product property={product}/>
+                <Product property={product} cart={addToCart}/>
             )
         })}
     </div>
